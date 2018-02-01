1c7156676df3c54fed2ff1f4e0f59a9a14df61ab
hqh546020152 <546020152@qq.com>
2018-02-01 11:27:55 +0800

    X install.sh

--- no_docker_install/install.sh
+++ no_docker_install/install.sh
@@ -1,70 +1,39 @@
-版本：5.4.3
-
-
-1、下载
+下载 Elasticsearch、Logstash、Kibana
+官方地址：https://www.elastic.co/cn/products
 cd  /usr/local
-wget        https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.4.3.tar.gz
-tar  -zxvf  elasticsearch-5.4.3.tar.gz
-备注：如下其他版本直接更换链接中的版本号即可
+wgethttps://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.1.tar.gz
+wget https://artifacts.elastic.co/downloads/logstash/logstash-5.6.1.tar.gz
+wget https://artifacts.elastic.co/downloads/kibana/kibana-5.6.1-linux-x86_64.tar.gz
+tar  -zxvfelasticsearch-5.6.1.tar.gz
+tar  -zxvf  logstash-5.6.1.tar.gz
+tar  -zxvf  kibana-5.6.1-linux-x86_64.tar.gz
 
-2、启动es
-useradd es
-sudo su - es -c  "/usr/local/elasticsearch-5.4.3/bin/elasticsearch -d"
-查看进程(占用端口：9200  9300)
-ps -ef |grep elastic
-备注：Elasticsearch不能使用root用户运行
+启动Elasticsearch
+Elasticsearch不能使用root用户运行
+useradd elk
+sudo su - elk -c  "/usr/local/elasticsearch-5.6.1/bin/elasticsearch"
+验证：curlhttp://localhost:9200
+启动Logstash
+创建配置文件logstash.conf 声明输入-input与输出-output、及文件格式定义-filter
 
-3、常见问题
-启动失败可以查看日志
-tail  -fn100  /usr/local/elasticsearch-5.4.3/logs/my-application.log
-可能查看到的日志如下：
-[2] bootstrap checks failed
-[1]: max file descriptors [4096] for elasticsearch process is too low, increase to at least [65536]
-[2]: max virtual memory areas vm.max_map_count [65530] is too low, increase to at least [262144]
+参考：
 
-问题：[1]: max file descriptors [4096] for elasticsearch process is too low, increase to at least [65536]
+input {
+      beats {
+            port => 5044
+      }
+}
+output {
+      stdout {
+            codec => rubydebug
+      }
+      elasticsearch {
+            hosts => ["192.168.12.235:9200"]
+      }
+}
 
-解决步骤：切换到root用户，编辑limits.conf 添加类似如下内容
-vi /etc/security/limits.conf 
-
-添加如下内容:
-* soft nofile 65536
-* hard nofile 131072
-* soft nproc 2048
-* hard nproc 4096
-
-问题：[2]: max virtual memory areas vm.max_map_count [65530] is too low, increase to at least [262144]
-解决：切换到root用户修改配置sysctl.conf
-
-vi /etc/sysctl.conf 
-添加下面配置：
-vm.max_map_count=655360
-并执行命令：
-sysctl -p
-重启启动elastic
-
-其他问题可以参考该链接：https://www.cnblogs.com/duanxuan/p/6473005.html
-
-4、配置文件详解：
-cat /usr/local/elasticsearch-5.4.3/config/elasticsearch.yml
-
-cluster.name: my-application
-#集群名称，同一集群该名称必须相同
-node.name: node-1
-#节点名称，同一集群的节点名称不能相同，如果不配置该项，系统会随机分配一个名称。
-node.attr.rack: r1
-#指定节点的部落属性，这是一个比集群更大的范围，一般不修改
-network.host: 0.0.0.0
-#设置端口监听对应的网络地址，一般设置为内网地址
-http.port: 9200
-#设置对外监听端口，默认9200，处于安全考虑，一般进行修改
-#path.data: /path/to/data
-#指定数据的存储路径  
-#path.logs: /path/to/logs
-#指定日志的存储路径
-discovery.zen.ping.unicast.hosts: ["10.40.11.141", "[::1]"]
-#节点发现设置，将集群中其他节点IP配置至此即可。默认使用tcp 9300端口通讯
-action.auto_create_index: .security,.monitoring*,.watches,.triggered_watches,.watcher-history*,.*
-
-5、数据迁移
-Elasticsearch同版本数据迁移，只需将数据存储目录的文件拷贝到新集群path，重启es集群，即可自动recovery，迁移效率最快。
+启动：/usr/local/logstash/bin/logstash  -f  ./logstash.conf
+启动Kibana
+/usr/local/kibana/bin/kibana
+验证：curlhttp://localhost:5601 
+至此安装完毕，具体配置需要在特定的场景进行调整。
